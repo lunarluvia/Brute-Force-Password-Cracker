550264a2775e58ebdb4a0f1638515c3da48acbb9
Lunar Luvia <124741915+lunarluvia@users.noreply.github.com>
2023-05-01 19:17:32 +0200

    Update main.py

--- Cracker/main.py
+++ Cracker/main.py
@@ -2,30 +2,30 @@ import itertools
 import time
 
 # Define characters to use in the password
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 # Define the password to be cracked
-password = "password123"
+PASSWORD = "password123"
 
 # Define the maximum password length to try
-max_length = 10
+MAX_LENGTH = 10
 
 # Track the start time of the password cracking process
 start_time = time.time()
 
-# Try all possible combinations of characters up to max_length
-for length in range(1, max_length + 1):
-    for combination in itertools.product(chars, repeat=length):
+# Try all possible combinations of characters up to MAX_LENGTH
+for length in range(1, MAX_LENGTH + 1):
+    for combination in itertools.product(CHARS, repeat=length):
         # Join the characters in the combination to form a password candidate
         candidate = "".join(combination)
-        print("Trying password:", candidate)
+        print(f"Trying password: {candidate}")
         # Check if the candidate matches the password
-        if candidate == password:
+        if candidate == PASSWORD:
             # Track the end time of the password cracking process
             end_time = time.time()
-            print("Password found:", candidate)
+            print(f"Password found: {candidate}")
             # Calculate the time taken to crack the password
             time_taken = end_time - start_time
-            print("Time taken:", time_taken, "seconds")
+            print(f"Time taken: {time_taken:.2f} seconds")
             # Terminate the password cracking process
             raise SystemExit
